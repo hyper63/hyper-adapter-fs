bbc269f7c4a58760f2edf98bb372515c2cc98d2b
Tyler Hall <tyler@hyper.io>
2022-08-30 20:58:31 +0000

    chore: bump deps and remove lockfile

--- deps.js
+++ deps.js
@@ -1,7 +1,7 @@
 export * as R from "https://cdn.skypack.dev/ramda@0.28.0";
 export { default as crocks } from "https://cdn.skypack.dev/crocks@0.12.4";
 
-export * as path from "https://deno.land/std@0.135.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.153.0/path/mod.ts";
 
 export {
   HyperErr,
--- dev_deps.js
+++ dev_deps.js
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertObjectMatch,
-} from "https://deno.land/std@0.127.0/testing/asserts.ts";
-export { v4 } from "https://deno.land/std@0.127.0/uuid/mod.ts";
-export { readAll } from "https://deno.land/std@0.127.0/io/util.ts";
+} from "https://deno.land/std@0.153.0/testing/asserts.ts";
+export { v4 } from "https://deno.land/std@0.153.0/uuid/mod.ts";
+export { readAll } from "https://deno.land/std@0.153.0/io/util.ts";
