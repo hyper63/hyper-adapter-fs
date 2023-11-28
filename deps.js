28445ff5cba625f1cf888ec565ed535a1603b858
Tyler Hall <tyler@hyper.io>
2023-11-28 18:19:31 -0500

    chore: bump dev deps and use npm in lieu of skypack

--- deps.js
+++ deps.js
@@ -1,9 +1,9 @@
-export * as R from 'https://cdn.skypack.dev/ramda@0.29.0'
-export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
+export * as R from 'npm:ramda@0.29.1'
+export { default as crocks } from 'npm:crocks@0.12.4'
 
-export * as path from 'https://deno.land/std@0.187.0/path/mod.ts'
+export * as path from 'https://deno.land/std@0.208.0/path/mod.ts'
 
 export {
   HyperErr,
   isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.2/packages/utils/hyper-err.js'
